d3921c32c121466d586ff4372b5811290d699eb0
Joao Pedro Araujo de Lima <jopearaujo94@gmail.com>
2023-10-11 23:04:43 -0300

    No arquivo config/routes.rb, defindo e configurando as rotas

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
-Rails.application.routes.draw do
+Rails.application.routes.draw do 
   resources :replies, only: [:create]
   resources :topics
 
   #Define a rota raiz como a página de listagem de tópicos
   root 'topics#index'
 end
+
