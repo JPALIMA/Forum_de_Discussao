da0204b7fac2252ce79b382c3cb704774d73e391
Joao Pedro Araujo de Lima <jopearaujo94@gmail.com>
2023-10-18 22:35:43 -0300

    configurando o arquivo routes.rb mantendo a definição da rota raiz apenas na seção de autenticação e remover a definição root 'devise/sessions#new'.

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,13 @@
 Rails.application.routes.draw do
-  #Rotas da área de administração
+  # Rotas da área de administração
   namespace :admin do
     get '/', to: 'admin#index', as: 'root'
+  end
 
-end
-
-#Restrição de autenticação para rotas principais
-authenticated :user do
-  root to: 'welcome#index' #Rota raiz do projeto principal após o login
-  resources :replies, only: [:create]
-  resources :topics
-end
-
-#Rota principal antes de fazer login
-  root 'devise/session#new'
+  # Restrição de autenticação para rotas principais
+  authenticated :user do
+    root to: 'welcome#index' # Rota raiz do projeto principal após o login
+    resources :replies, only: [:create]
+    resources :topics
+  end
 end
