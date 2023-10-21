6f10d858d2528a4ce6a3998e1e980ed6d6fb4e22
Joao Pedro Araujo de Lima <jopearaujo94@gmail.com>
2023-10-21 17:48:16 -0300

    definindo as autorizações que funcionarão para o modelo app/policies/topic_policy.rbTopic. especificando quais usuários têm permissão para fazer o quê com os tópicos

--- app/policies/topic_policy.rb
+++ app/policies/topic_policy.rb
@@ -1,5 +1,18 @@
-class TopicPolicy < ApplicationPolicy
-  def show?
-    true #qualquer usário autenticado pode ver tópico
+class TopicPolicy
+  attr_reader :user, :topic
+
+  def initialize(user, topic)
+    @user = user
+    @topic = topic
+  end
+
+  def update?
+    #Permite que um usuário atualize um tópico se ele for o autor do tópico
+    user == topic.user
+  end
+
+  def destroy?
+    #Permite que um usuário exclua um tópico se ele for o autor do tópico
+    user == topic.user
   end
 end
