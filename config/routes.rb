0f1df710a66df59c05bbb2a41f5fe74e04a2761b
Joao Pedro Araujo de Lima <jopearaujo94@gmail.com>
2023-10-20 22:51:36 -0300

    corregindo erro relacionado ao código no arquivo config/routes.rb, que ainda contém uma chamada para authenticated que não é reconhecida na versão do Rails que está usando (7.0.8). usando agora uma restrição lambda que verifica se um usuário autenticado está presente na sessão do Warden

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 # Restrição de autenticação para rotas principais
-authenticated :user do
+constraints ->(request) { request.env["warden"].authenticate? and request.env["warden"].user.is_a? User } do
   root to: 'welcome#index', as: :authenticated_root
   resources :replies, only: [:create]
   resources :topics
