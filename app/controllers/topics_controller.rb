5c6c7b9b6e3227e4380b4f7ce0ee8895be61e35b
Joao Pedro Araujo de Lima <jopearaujo94@gmail.com>
2023-10-21 17:53:00 -0300

    Autorização no controlador: No controlador onde vai verificar as permissões (por exemplo, o controlador que lida com as ações de atualização e exclusão), usará o método authorize. Por exemplo, no método update

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -12,6 +12,19 @@ class TopicsController < ApplicationController
     @topic = Topic.new
   end
 
+  def update
+    @topic = Topic.find(params[:id])
+    authorize @topic #Verifica se o usuário tem permissão para atualizar o tópico
+
+    #Se o usuário tiver permissão, continue com a atualização
+    if @topic.update(topic_params)
+      #Redirecione ou faça algo após a atualização bem-sucedida
+    else
+      #Lidere com erros, caso haja algum
+    end
+  end
+
+
   def create
     @topic = Topic.new(topic_params)
 
