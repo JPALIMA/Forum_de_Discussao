266c1090c1eb02995e4a24d9540449553534a385
Joao Pedro Araujo de Lima <jopearaujo94@gmail.com>
2023-10-21 13:46:28 -0300

    comentar as rotas no 'config/routes.rb' temporariamente as restrições para permitir que o comando rails generate pundit:install

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 # Restrição de autenticação para rotas principais
-constraints ->(request) { request.env["warden"].authenticate? and request.env["warden"].user.is_a? User } do
-  root to: 'welcome#index', as: :authenticated_root
-  resources :replies, only: [:create]
-  resources :topics
-end
+#constraints ->(request) { request.env["warden"].authenticate? and request.env["warden"].user.is_a? User } do
+#  root to: 'welcome#index', as: :authenticated_root
+#  resources :replies, only: [:create]
+#  resources :topics
+#end
