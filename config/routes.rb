5c3b1a86501a75946cdbada4e5ea9b70a3e2e17f
Joao Pedro Araujo de Lima <jopearaujo94@gmail.com>
2023-10-20 22:39:03 -0300

    exemplo, a rota raiz após o login é definida como 'welcome#index'. Você pode substituir 'welcome#index' pelo controlador e ação que você deseja usar como página inicial após o login.

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,6 @@
-Rails.application.routes.draw do
-  # Rotas da área de administração
-  namespace :admin do
-    get '/', to: 'admin#index', as: 'root'
-  end
-
-  # Restrição de autenticação para rotas principais
-  authenticated :user do
-    root to: 'welcome#index' # Rota raiz do projeto principal após o login
-    resources :replies, only: [:create]
-    resources :topics
-  end
+# Restrição de autenticação para rotas principais
+authenticated :user do
+  root to: 'welcome#index', as: :authenticated_root
+  resources :replies, only: [:create]
+  resources :topics
 end
