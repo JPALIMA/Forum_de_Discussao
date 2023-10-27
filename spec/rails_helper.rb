2160b892c11162abea95e1fbe10e8c48887042b6
Joao Pedro Araujo de Lima <jopearaujo94@gmail.com>
2023-10-26 23:52:02 -0300

    ajustando o arquivo spec/rails_helper.rb (ou spec/spec_helper.rb, dependendo da versão do RSpec) requer o 'FactoryBot'

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,5 +1,7 @@
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 require 'spec_helper'
+#Aplicanto no arquivo spec/rails_helper.rb (ou spec/spec_helper.rb, dependendo da versão do RSpec) requer o 'FactoryBot'.
+require 'factory_bot'
 ENV['RAILS_ENV'] ||= 'test'
 require_relative '../config/environment'
 # Prevent database truncation if the environment is production
