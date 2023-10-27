4411824c290dcd3544823b0ba38d345475a990ba
Joao Pedro Araujo de Lima <jopearaujo94@gmail.com>
2023-10-26 23:37:10 -0300

    definidas no arquivo de fábricas em spec/factories/users.rb

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,7 +1,10 @@
+# spec/factories/users.rb
 FactoryBot.define do
      factory :user do
-          name { "jão"}
-          email { "jão@exemplo.com"}
-          password { "senha123"}
+       name { "exemplo" } # Use Faker para gerar nomes aleatórios
+       email { "exemplo@example.com" } # Use Faker para gerar emails aleatórios
+       password { "senha123" } # Você pode definir uma senha padrão, se necessário
      end
 end
+
+   
