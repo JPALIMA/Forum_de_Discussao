82c9f55468fa9f70d4f198b5ca0fe58dcd34ef02
Joao Pedro Araujo de Lima <jopearaujo94@gmail.com>
2023-10-27 21:27:28 -0300

    ajuste  na fábrica User para refletir o atributo full_name

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,10 +1,9 @@
 # spec/factories/users.rb
 FactoryBot.define do
      factory :user do
-       name { "exemplo" } # Use Faker para gerar nomes aleatórios
-       email { "exemplo@example.com" } # Use Faker para gerar emails aleatórios
-       password { "senha123" } # Você pode definir uma senha padrão, se necessário
+          full_name { "exemplo"} #Use Faker para gerar nomes aleatórios
+          email { "exemplo@exemple.com"} #Use Faker para gerar emails aleatórios
+          password { "senha123" } #Você pode definir uma senha padrão, se necessário
      end
 end
-
    
