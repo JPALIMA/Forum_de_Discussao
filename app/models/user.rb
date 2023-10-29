9a310becc0c05af94ecd538657c3890ac06fcb2b
Joao Pedro Araujo de Lima <jopearaujo94@gmail.com>
2023-10-28 22:44:25 -0300

    erro ao corregar ao atributo 'name'. definindo modelo incluindo na classe 'User' o atributo 'name'.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,8 @@
-# app/models/user.rb
+#app/models/user.rb
 class User < ApplicationRecord
      validates :name, presence: true
      validates :email, presence: true
      validates :password, presence: true
-   
-     # Outros atributos e associações do usuário
+
+     #Outros atributos e associações do usuário
 end
-   
