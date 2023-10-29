84d2941e473c770d9b94e982a7eedf17ab243344
Joao Pedro Araujo de Lima <jopearaujo94@gmail.com>
2023-10-28 22:46:43 -0300

    erro persiste devido ao atributo full_name no modelo 'User', colocando o atributo 'name'.

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,8 +1,9 @@
 # spec/factories/users.rb
 FactoryBot.define do
      factory :user do
-          full_name { "exemplo"} #Use Faker para gerar nomes aleatórios
-          email { "exemplo@exemple.com"} #Use Faker para gerar emails aleatórios
-          password { "senha123" } #Você pode definir uma senha padrão, se necessário
+          name {"exemplo"} #User faker para gerar nomes aleatórios
+          email {"exemplo@exemple.com"} #Uso para gerar emails aleatórios
+          password {"senha123"} #definir uma senha padrão
      end
 end
+
